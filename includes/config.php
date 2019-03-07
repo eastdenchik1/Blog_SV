2fa45e570dbb4ac9bd423737e865d06ba55ce857
eastdenchik1 <32023322+eastdenchik1@users.noreply.github.com>
2019-03-07 23:12:51 +0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,27 @@
 <?php
 
 
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+
+$config = array (
+    'title' => 'Блог Дениса Свиридова',
+    'VK_URL' => 'https://vk.com/westdenchik',
+    'users' => array(
+        'login' => 'admin',
+        'password' => '1234'
+    ),
+    'db' => array(
+        'server' => $url["host"],
+        'user' => $url["user"],
+        'password' => $url["pass"],
+        'dbname' => substr($url["path"],1)
+    ),
+);
+
+
+
+/*
 $config = array (
     'title' => 'Блог Дениса Свиридова',
     'VK_URL' => 'https://vk.com/badwolf4500',
@@ -15,5 +36,11 @@ $config = array (
         'dbname' => 'blog_course'
     ),
 );
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1);
+*/
+
 
 require "db.php";
